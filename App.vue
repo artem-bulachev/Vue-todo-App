77b87debf6a48ea8ba4651a3b0c1dc698a139b05
artem-bulachev <48219016+artem-bulachev@users.noreply.github.com>
2019-04-03 00:14:07 +0500

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-card-title primary-title class="title">
+          <div>
+            <h3 class="mat-h2 mb-0">Todos</h3>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <TodoItem v-for="(todo, index) in todos"
+                    :key="index"
+                    :todo="todo">
+          </TodoItem>
+          <InputField ref="todoInput"></InputField>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-app>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+import InputField from './components/InputField'
+
+export default {
+  name: 'App',
+  components: {
+    TodoItem,
+    InputField
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    todos () {
+      return this.$store.state.todos
+    }
+  },
+  methods: {
+    addTodo () {
+      this.$refs.todoInput.addTodo()
+    }
+  }
+}
+</script>
+
+<style>
+  .title {
+    display: flex;
+    justify-content: center;
+  }
+</style>
